4e261a65ab19f51fd7cd4397e89959a4c327ae15
weicheng lee <z82111712@gmail.com>
2021-11-16 09:43:27 -0800

    update factorial, fibonacci

---
+++ recursion2.js
@@ -0,0 +1,107 @@
+/*
+  Recursive Factorial
+  Input: integer
+  Output: integer, product of ints from 1 up to given integer
+
+  If less than zero, treat as zero.
+  Bonus: If not integer, truncate (remove decimals).
+
+  Experts tell us 0! is 1.
+
+  rFact(3) = 6 (1*2*3)
+  rFact(6.8) = 720 (1*2*3*4*5*6)
+*/
+
+const num1 = 3;
+const expected1 = 6;
+// Explanation: 1*2*3 = 6
+
+const num2 = 6.8;
+const expected2 = 720;
+// Explanation: 1*2*3*4*5*6 = 720
+
+const num3 = 0;
+const expected3 = 1;
+
+/**
+ * Recursively multiples 1 to the given int.
+ * - Time: O(?).
+ * - Space: O(?).
+ * @param {number} n The int to factorial. Treat negatives as zero and
+ *    floor decimals.
+ * @returns {number} The result of !n.
+ */
+function factorial(n) {
+    if (n < 1) {
+        console.log(`--------STOP at number ${n}-------`)
+        return 1
+    } else {
+        console.log(`Math.floor(${n}) * factorial(${n - 1})`)
+        return Math.floor(n) * factorial(n - 1)
+    }
+}
+
+/*****************************************************************************/
+
+/*
+  Return the fibonacci number at the nth position, recursively.
+
+  Fibonacci sequence: 0, 1, 1, 2, 3, 5, 8, 13, 21, 34, ...
+  The next number is found by adding up the two numbers before it,
+  starting with 0 and 1 as the first two numbers of the sequence.
+*/
+
+const num1 = 0;
+const expected1 = 0;
+
+const num2 = 1;
+const expected2 = 1;
+
+const num3 = 2;
+const expected3 = 1;
+
+const num4 = 3;
+const expected4 = 2;
+
+const num5 = 4;
+const expected5 = 3;
+
+const num6 = 8;
+const expected6 = 21;
+
+/**
+ * Recursively finds the nth number in the fibonacci sequence.
+ * - Time: O(?).
+ * - Space: O(?).
+ * @param {number} num The position of the desired number in the fibonacci sequence.
+ * @returns {number} The fibonacci number at the given position.
+ */
+function fibonacci(num) {
+    if (num === 0) {
+        console.log("-----------STOP POINT-----------")
+        return 0;
+    } else if (num === 1) {
+        return 1;
+    } else {
+        console.log(`indexOne - ${num-1} indexTwo - ${num-2}`)
+        return fibonacci(num-1) + fibonacci(num-2)
+    }
+}
+
+// console.log(fibonacci(num1))
+// console.log(fibonacci(num2))
+// console.log(fibonacci(num3))
+// console.log(fibonacci(num4))
+// console.log(fibonacci(num5))
+// console.log(fibonacci(num6))
+
+/*
+0, 1, 1, 2, 3, 5, 8, 13, 21, 34
+f(0) = 0
+f(1) = 1
+f(2) = 1
+f(3) = f(2) + f(1)
+f(4) = f(3) + f(2)
+f(5) = f(4) + f(3)
+f(6) = f(5) + f(4)
+*/
