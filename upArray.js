38732fa40a575a8fbbbd719d37f69b305fd4cf4c
weicheng lee <z82111712@gmail.com>
2021-11-23 15:14:10 -0800

    update uparray

--- upArray.js
+++ upArray.js
@@ -1,14 +1,19 @@
 function upArray(arr){
-    // ...
     if (arr.length === 0) return null;
 
     for (let i = 0; i < arr.length; i++) {
         if (arr[i] < 0 || arr[i].toString().length !== 1) {
-        return null;
+            return null;
         }
     }
 
-    let arrIncreaseByOne = (parseInt(arr.join(""))+1).toString().split("");
+    if (arr.length <= 14) {
+        arrIncreaseByOne = (parseInt(arr.join(""))+1).toString().split("");
+    } else {
+        const strOfNum = arr.join("")
+
+        arrIncreaseByOne = (strOfNum.slice(0, strOfNum.length - 8) + (parseInt(strOfNum.slice(-8)) + 1).toString()).split("")
+    }
 
     return arrIncreaseByOne.map(element => parseInt(element));
 }
