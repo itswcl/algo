4d6134a0471548832b4a795127918819413955d0
weicheng lee <z82111712@gmail.com>
2022-01-11 10:05:58 -0800

    add second with while loop

--- bubbleSort.js
+++ bubbleSort.js
@@ -18,8 +18,8 @@ const nums3 = [10, 9, 8, 7, 6, 5, 4, 3, 2, 1];
 
 function bubbleSort(nums) {
 
-    for (let i = 0; i < nums.length - 1; i++) {
-
+    for (let i = 0; i < nums.length; i++) {
+        // stop at
         for (let j = 0; j < nums.length - i - 1; j++) {
             const temp = nums[j];
             if (nums[j] > nums[j + 1]) {
@@ -35,6 +35,24 @@ function bubbleSort(nums) {
     console.log(nums);
 }
 
-bubbleSort(nums1)
-bubbleSort(nums2)
-bubbleSort(nums3)
+function bubbleSort2(nums) {
+    let isSorted = false; // flag to check if things are sorted or not.
+
+    while (isSorted === false) { // we want to run the bubble sorting as long as our flag says the array is not yet sorted
+        isSorted = true; // we'll flip the flag to true, as default
+
+        for (let i = 0; i < nums.length - 1; i++) { // length - 1 bc we set j to i+1
+            const j = i + 1; // check value at the current index with the value at the next index
+
+            if (nums[i] > nums[j]) { // if the current index's value is ( > ) greater than the next value
+                isSorted = false; // flip the flag
+                // swap
+                const temp = nums[i];
+                nums[i] = nums[j];
+                nums[j] = temp;
+            }
+        }
+    }
+    console.log(nums);
+    return nums; // by this time it's sorted
+}
