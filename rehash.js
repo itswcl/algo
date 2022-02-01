fb3eec900a447d62f84c3931c30519bce156c73c
weicheng lee <z82111712@gmail.com>
2022-02-01 10:15:02 -0800

    update rehash

--- rehash.js
+++ rehash.js
@@ -15,7 +15,7 @@ const str1 = "b70a164c32a20c10";
 const expected1 = "a184b70c42";
 
 // step 1:
-frequencyMap = {}
+// frequencyMap = {}
 // step 2:
 // extract [keys? values? something?]
 // if it's an array -> sort
@@ -29,7 +29,48 @@ frequencyMap = {}
 // isNaN(x)
 // myObj.hasOwnProperty("key")
 
-function rehash(str) { }
+function rehash(str) {
+  // hash the map
+  let frequencyMap = {}
+  // for result
+  let frequencyArr = []
+
+  // iterate the str
+  for (let i = 0; i < str.length; i++) {
+    const target = str[i];
+    // check if it's letter and not in map yet
+    if (isNaN(target) && frequencyMap[target] === undefined) {
+      frequencyMap[target] = 0;
+      // if it's number
+    } else if (!isNaN(target)) {
+      // set the key as pervious element
+      let key = str[i - 1]
+      // current index is first number
+      let j = i;
+      // store the continue number as string
+      let tempNum = "";
+
+      // check next element if it's number
+      while (!isNaN(str[j])) {
+        // as long as number we add to number String
+        tempNum += str[j];
+        // move to next index
+        j++
+      }
+      // once the next index is letter we stop and add to the map
+      frequencyMap[key] += parseInt(tempNum);
+      // and start the outer loop from last element - 1
+      // because we incremental 1 outer loop
+      i = j - 1
+    }
+  }
+  // turn the map into array
+  for (let key in frequencyMap) {
+    frequencyArr.push(key + frequencyMap[key])
+  }
+  // return as whole arr as string
+  return frequencyArr.sort().join("")
+}
 
 rehash(str1);
 console.log(rehash(str1) === expected1, "<-- should be \"true\"");
