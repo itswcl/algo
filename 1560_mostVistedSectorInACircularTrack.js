2cfa3bf064a3c08b2efbc478f73da6742e89fe8b
weicheng lee <z82111712@gmail.com>
2022-02-10 10:50:50 -0800

    initial most visited point

---
+++ 1560_mostVistedSectorInACircularTrack.js
@@ -0,0 +1,18 @@
+/*
+Given an integer n and an integer array rounds. We have a circular
+track which consists of n sectors labeled from 1 to n. A marathon
+will be held on this track, the marathon consists of m rounds. The
+ith round starts at sector rounds[i - 1] and ends at sector rounds[i]
+. For example, round 1 starts at sector rounds[0] and ends at sector
+rounds[1]
+*/
+
+// Input: n = 4, rounds = [1,3,1,2] Output: [1,2]
+// Input: n = 7, rounds = [1,3,5,7] Output: [1,2,3,4,5,6,7]
+
+
+const mostVisited = (nth, rounds) => {
+    let mostVisitedPoint = []
+
+
+}
