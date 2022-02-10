16a34ca56578a93daf3cdfa83f497c8be14c0bd5
weicheng lee <z82111712@gmail.com>
2022-02-10 11:01:56 -0800

    update most viiste point

--- 1560_mostVistedSectorInACircularTrack.js
+++ 1560_mostVistedSectorInACircularTrack.js
@@ -12,7 +12,26 @@ rounds[1]
 
 
 const mostVisited = (nth, rounds) => {
+    // initial result
     let mostVisitedPoint = []
+    // set start and end point
+    const startPoint = rounds[0]
+    const endPoint = rounds[rounds.length - 1]
 
-
+    // if start is less or equal to end point we can assume we been thru
+    // the point from start to end each time
+    if (startPoint <= endPoint) {
+        for (let i = startPoint; i <= endPoint; i++) {mostVisitedPoint.push(i)}
+    // if start from large number
+    } else {
+        // whatever less than endpoint we been thru
+        for (let i = 1; i <= endPoint; i++) {mostVisitedPoint.push(i)}
+        // and the first start point to the n cuz start point always less than n
+        for (let i = startPoint; i <= nth; i++) {mostVisitedPoint.push(i)}
+    }
+    return mostVisitedPoint
 }
+
+console.log(mostVisited(9, [1,2,3]))
+console.log(mostVisited(2, [2,1,2,1,2]))
+console.log(mostVisited(3, [1,2,3,2]))
