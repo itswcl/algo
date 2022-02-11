dcaa67fd44ac626e273320bd89d41d369d2802b4
weicheng lee <z82111712@gmail.com>
2022-02-11 14:22:46 -0800

    update check inclusion

--- 567_permutationInString.js
+++ 567_permutationInString.js
@@ -1,37 +1,42 @@
-/**
- * @param {string} s1
- * @param {string} s2
- * @return {boolean}
- */
-var checkInclusion = function (s1, s2) {
-    // iterate thru s2
-    for (let i = 0; i < s2.length; i++) {
-        const meetPoint = s2[i]
 
-        // if meet the letter from s1 in last element
-        if (meetPoint === s1[s1.length - 1]) { // b
-            let s2Idx = i + 1; // 4
-            for (let j = s1.length - 2; j >= 0; j--) { // j = 0
-                const letterS1 = s1[j] // a
-
-                if (letterS1 !== s2[s2Idx]) { // a !== a
-                    return false;
-                }
-                s2Idx++
-
-            }
-            return true
-
-        } else if (meetPoint === s1[0]) {
-            for (let j = 1; j < s1.length; j++) {
-                const letterS1 = s1[j]
-
-                let s2Idx = i + j
-                if (letterS1 !== s2[s2Idx]) {
-                    return false;
-                }
-            }
-            return true;
-        }
+var checkInclusion = function(s1, s2) {
+    // set the letter map from s1
+    const str1Map = {};
+    const str1Length = s1.length;
+    for (let i = 0 ; i < str1Length; i++) {
+        const letter = s1[i];
+        str1Map[letter] = (str1Map[letter] || 0) + 1;
     }
+
+    // track MatchMap for str2 and match with str1Map
+    const matchMap = {};
+    const str2Length = s2.length;
+
+    // slide window with pointer
+    let left = 0;
+    let right = 0;
+
+    // checking til we meet the entire s2
+    while (right < str2Length) {
+        const letterS2 = s2[right]
+        // building the map
+        matchMap[letterS2] = (matchMap[letterS2] || 0) + 1;
+
+        // now we compare if it exist in str1Map or more than S1
+        while(matchMap[letterS2] > (str1Map[letterS2] || 0)) {
+            // we will deduct count by 1
+            matchMap[s2[left]]-=1;
+            // raise the left pointer
+            left++;
+        }
+
+        // if we get the length from right and left then it is true and early exist
+        if (right - left + 1 === str1Length) return true;
+
+        // if not meet yet we raise right pointer
+        right++
+    }
+    return false;
 };
+
+console.log(checkInclusion("aba", "eidabbaooo"));
