de1d9438eab55b9f9be1c2a3fceb892a0ee65f5f
weicheng lee <z82111712@gmail.com>
2022-02-15 10:08:50 -0800

    update divide conquer with comment

--- 53_maximumSubarray.js
+++ 53_maximumSubarray.js
@@ -2,8 +2,8 @@
  * @param {number[]} nums
  * @return {number}
  */
- var maxSubArray = function(nums) {
-    return findMax(nums, startPoint = 0, endPoint = nums.length-1)
+var maxSubArray = function (nums) {
+    return findMax(nums, startPoint = 0, endPoint = nums.length - 1)
 };
 
 const findMax = (nums, leftSide, rightSide) => {
@@ -12,11 +12,15 @@ const findMax = (nums, leftSide, rightSide) => {
         return nums[leftSide]
     }
 
+    // [X,X,X,MIDPOINT,X,X,X]
     let midPoint = Math.floor((leftSide + rightSide) / 2)
 
+    // [X,X,X,MID]
     let leftSum = findMax(nums, leftSide, midPoint);
-    let rightSum = findMax(nums, midPoint+1, rightSide);
+    // [X,X,X]
+    let rightSum = findMax(nums, midPoint + 1, rightSide);
 
+    // [X,X,X,MIDPOINT,X,X,X]
     let crossSum = findCrossSum(nums, leftSide, midPoint, rightSide)
     return Math.max(leftSum, rightSum, crossSum)
 }
@@ -24,23 +28,32 @@ const findMax = (nums, leftSide, rightSide) => {
 const findCrossSum = (nums, leftSide, midpoint, rightSide) => {
     let sum = 0;
 
+    // cant use 0 here because we have negative number in array
     let maxLeftSum = -Infinity;
     // the direction for the loop is
-    // <<<<<<<<<<<<<<<
+    // <<<<<<<<<<<<<<<Start from midpoint and count backward
     for (let i = midpoint; i >= leftSide; i--) {
+        // add up the number
         sum += nums[i];
+        // compare the updated sum with maxLeftSum
+        // update maxLeftSum with updated sum if larger
         maxLeftSum = Math.max(maxLeftSum, sum);
     }
 
     // reset the sum for right side
     sum = 0;
+    // cant use 0 here because we have negative number in array
     let maxRightSum = -Infinity;
     // the direction for the loop is
-    // >>>>>>>>>>>>>>>>>
-    for (let i = midpoint+1; i <= rightSide; i++) {
+    // start from midpoint and count forward >>>>>>>>>>>>>>>>>
+    for (let i = midpoint + 1; i <= rightSide; i++) {
+        // update sum
         sum += nums[i];
+        // compared updated sum and maxRightSum
+        // if larger then update
         maxRightSum = Math.max(maxRightSum, sum)
     }
 
+    // add up both left and right for crossSum
     return maxLeftSum + maxRightSum;
 }
