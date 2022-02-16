09b9b768fd6dba93e79971e34969e38be4f56413
weicheng lee <z82111712@gmail.com>
2022-02-16 15:33:33 -0800

    adding comment

--- 704_binarySearch.js
+++ 704_binarySearch.js
@@ -4,21 +4,31 @@
  * @return {number}
  */
 const search = (nums, target) => {
-    // setup and reset left and right index depends compareson between target and midIdx
+    // setup and reset left and right index depends comparison between target and midIdx
 
     let leftIdx = 0;
     let rightIdx = nums.length - 1;
 
+    // as long as left index not crossing right index
     while (leftIdx <= rightIdx) {
-        let midIdx = Math.floor((rightIdx - leftIdx) / 2 + leftIdx)
+        // Mid pointer between left to right
+        let midPointer = Math.floor((rightIdx - leftIdx) / 2 + leftIdx)
 
-        if (nums[midIdx] === target) return midIdx;
+        // end case, once mid pointer value is the target
+        if (nums[midPointer] === target) return midPointer;
 
-        if (nums[midIdx] < target) {
-            leftIdx = midIdx + 1;
+        // if the mid pointer value is less than target
+        // meaning that the left from mid are not what we look for
+        // we move our left to midpoint + 1 as new left
+        if (nums[midPointer] < target) {
+            leftIdx = midPointer + 1;
+        // if the mid pointer value is greater than target
+        // meaning that right from mid are not what we look for
+        // we move our right to midpoint - 1 as new right
         } else {
-            rightIdx = midIdx - 1;
+            rightIdx = midPointer - 1;
         }
     }
+    // if we dont find our target we return -1
     return -1
 };
