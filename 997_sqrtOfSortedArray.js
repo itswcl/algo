e34e82ff979b38adda37b831fa20b9d72fb6c7ab
weicheng lee <z82111712@gmail.com>
2022-02-21 15:57:47 -0800

    update sqrt of nums sorted

--- 997_sqrtOfSortedArray.js
+++ 997_sqrtOfSortedArray.js
@@ -9,3 +9,19 @@ var sortedSquares = function (nums) {
     }).sort(function (a, b) { return a - b });
 };
 
+const sortedSqrtNums = (nums) => {
+    const result = new Array(nums.length);
+    let leftIdx = 0;
+    let rightIdx = nums.length - 1;
+
+    for (let i = nums.length - 1; i >= 0; i--) {
+        if (Math.abs(nums[leftIdx]) < Math.abs(nums[rightIdx])) {
+            result[i] = nums[rightIdx] ** 2;
+            right--;
+        } else {
+            result[i] = nums[leftIdx] ** 2;
+            left++;
+        }
+    }
+    return result;
+}
