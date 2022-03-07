012da9f8ad26c6145a666c19e168bc840edce74c
weicheng lee <z82111712@gmail.com>
2022-03-07 12:32:36 -0800

    comment on searching rotate sorted array

--- Binary_Search/33_searchInRotatedSortedArray.js
+++ Binary_Search/33_searchInRotatedSortedArray.js
@@ -3,26 +3,33 @@
  * @param {number} target
  * @return {number}
  */
+
+// [4,5,6,7,0,1,2], 0
 var search = function (nums, target) {
     // left and right index
     let leftIdx = 0;
     let rightIdx = nums.length - 1;
 
-    // treverse til left > right
-    while (leftIdx <= rightIdx) {
-        const midPointer = Math.floor((rightIdx - leftIdx) / 2) + leftIdx
-        if (nums[midPointer] === target) {
+    // traverse til left > right
+    while (leftIdx <= rightIdx) { // 0 <= 6
+        const midPointer = Math.floor((rightIdx - leftIdx) / 2) + leftIdx // 3
+        if (nums[midPointer] === target) { // 7 === 0
             return midPointer;
 
-            // check if mid element is larger than start
-        } else if (nums[midPointer] >= nums[leftIdx]) {
-            if (nums[leftIdx] <= target && target < nums[midPointer]) {
+            // check if the numbers is ascending
+        } else if (nums[midPointer] >= nums[leftIdx]) { // nums[3] - 7 >= nums[0] - 4
+            // check if the target on the left side
+            if (nums[leftIdx] <= target && target < nums[midPointer]) { // nums[0] <= 0 && 0 < nums[3]
                 rightIdx = midPointer - 1;
+                // if one condition failed it's in right side
             } else {
                 leftIdx = midPointer + 1;
             }
 
+
+            // else it's descending
         } else {
+            // check if number in the right side
             if (nums[rightIdx] >= target && target > nums[midPointer]) {
                 leftIdx = midPointer + 1;
             } else {
--- Binary_Search/backGround.md
+++ Binary_Search/backGround.md
@@ -1,8 +1,15 @@
 # pre-processing
+    - sort
 # binary search
+    - loop or recursion to divide search space in half in each comparison
 # post processing
+    - viable candidates in remaining space
 
 # template I
+    - initial condition, left = 0 right = length - 1
+    - termination left > right
+    - searching left, right = mid - 1
+    - searching right, left = mid + 1
 ### Sqrt(x), guess number higher or lower, search in rotated sorted array
 ```js
 let left = 0; let right = nums.length - 1;
