44218952d7e648ea07b5152f39298f80faeb45b4
weicheng lee <z82111712@gmail.com>
2022-04-22 17:05:30 -0700

    update 3 sum

--- Array_String/15_ThreeSum.js
+++ Array_String/15_ThreeSum.js
@@ -4,40 +4,38 @@
  */
 
 const threeSum = (nums) => {
-    // sort the numbers
-    nums.sort((a, b) => a - b);
+    // first sort the array
+    nums.sort((a, b) => a - b)
     const res = [];
-    // need at least 2 numbers so length - 2
-    for (let i = 0; i < nums.length - 2; i++) {
-        const num = nums[i]
-        // num positive so no need to check the rest
-        if (num > 0) return res;
-        // need at least run once and check current and pervious then we skip
-        if (i > 0 && num === nums[i - 1]) continue;
 
-        // run the twoSum pointers here
-        // start from i + 1 because it's sorted
-        let left = i + 1, right = nums.length - 1
+    for (let i = 0; i < nums.length; i++) {
+        const num = nums[i];
+        if (num > 0) return res; // end function if first element is positive we wont find 0 ever
+
+        if (i > 0 && nums[i] === nums[i - 1]) continue; // we skip the element because duplicate triplets need to avoid
+
+        // initial left from next element and right
+        let left = i + 1, right = nums.length - 1;
         while (left < right) {
             const sum = num + nums[left] + nums[right]
+
+            // if we found our target sum
             if (sum === 0) {
-                // if we meet the target
-                // 1. push the target numbers into result arr
+                // add to result
                 res.push([num, nums[left], nums[right]])
-                // 2. update the pointers left and right
-                    // 1. check the next left if same then skip
-                while(nums[left] === nums[left + 1]) left++;
-                    // 2. check the next right if same then skip
-                while(nums[right] === nums[right - 1]) right--;
-                // otherwise left increase by 1 and right decrease by 1
+
+                // now we need to skip same element both left move up by 1 and right move down by 1
+                while (nums[left] === nums[left + 1]) left++;
+                while (nums[right] === nums[right - 1]) right--;
+                // once we find the end we need to move 1 positive for both site (the while loop stop at same element)
                 left++;
                 right--;
             } else if (sum > 0) {
-                // greater than target so right too big
-                right--;
+                // if sum is positive means we have larger right we need to move 1 down
+                right--
             } else {
-                // lesser than target so left too small
-                left++;
+                // if sum is negative means we have smaller left we need move 1 up
+                left++
             }
         }
     }
