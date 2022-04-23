32b40dee04e783ff5fb7f036325e056c017efff4
weicheng lee <z82111712@gmail.com>
2022-04-23 15:42:05 -0700

    review reoder list

--- LinkLists/143_reorderList.js
+++ LinkLists/143_reorderList.js
@@ -12,36 +12,39 @@
 // 1, 2, 3, 4       // 1, 2, 3, 4, 5     // aabb
 // 1, 4, 2, 3       // 1, 5, 2, 4, 3     // abab
 var reorderList = function (head) {
-    // 1. find middle list
-        // run 2 pointers first and second
-        let first = head, second = head;
-        // first.next meet null we stop
-        while (first && first.next) {
-            // we run first twice faster then second
-            first = first.next.next;
-            second = second.next
-        }
-        // second is our mid list
-    // 2. reverse middle list
-        // prev as null and current is second
-        let prev = null, current = second;
-        // iterate thur til current meet null
-        while(current) {
-            let next = current.next;
-            current.next = prev;
-            prev = current;
-            current = next;
-        }
-    // 3. merge first and prev list
-        first = head, second = prev;
-        while(second.next) {
-            // first to move our first next
-            let next = first.next;
-            first.next = second;
-            first = next;
-            // second to more our second next
-            next = second.next;
-            second.next = first;
-            second = next;
-        }
+    // first to split into 2 list
+    let first = head, second = head;
+    // stop when first reach the end of list
+    // because first is 2 time of speed of second
+    while (first && first.next) {
+        first = first.next.next;
+        second = second.next;
+    }
+
+    // then we reverse the second list
+    let prev = null;
+    let current = second;
+    while (current) {
+        let next = current.next;
+        current.next = prev;
+        prev = current;
+        current = next;
+    }
+    // then we find out reverse second list as "prev" because => >>>null to null<<<
+
+    // and we merge our "first portion of list" with "prev" as our second part of list
+    first = head, second = prev;
+    console.log(head, prev)
+    // [1,2,3] => [1,2], [3,2] we will only use if second.next exist
+    // because the last element will be same as the last element in first we dont need
+    while (second.next) {
+        let next = first.next;
+        first.next = second;
+        first = next;
+
+        next = second.next;
+        second.next = first;
+        second = next;
+
+    }
 }
