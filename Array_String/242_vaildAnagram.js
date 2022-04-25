228da2c92d7ecc08dbd540378802ec03183edb60
weicheng lee <z82111712@gmail.com>
2022-04-25 10:58:33 -0700

    review vaildAnagram

--- Array_String/242_vaildAnagram.js
+++ Array_String/242_vaildAnagram.js
@@ -5,6 +5,24 @@
  */
 var isAnagram = function (s, t) {
     return s.split("").sort().join("") === t.split("").sort().join("");
+// -----------------------------------------------------------------------------
+
+    // // if length differ then it's not anagram
+    // if (s.length !== t.length) return false;
+
+    // // default a hashmap to create letter counter from string s
+    // let map = {};
+    // for (let string of s) map[string] ? map[string]++ : map[string] = 1;
+
+    // for (let string of t) {
+    //     // if the letter not in map then it is not anagram
+    //     if (!map[string]) return false;
+    //     // then we start to decrementing each letter from string t
+    //     map[string]--
+    // }
+
+    // return true;
+// -----------------------------------------------------------------------------
     //     const letterMap = {}
     //     const st = s + t
     //     if (s.length !== t.length) return false;
